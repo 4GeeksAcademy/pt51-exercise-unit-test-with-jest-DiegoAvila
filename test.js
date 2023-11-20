9b0990ed66940f8ec9c10bc4fbcbb6531554c140
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2023-11-20 20:29:19 +0000

    modificacion para pasar el expectec de string a number

--- test.js
+++ test.js
@@ -15,37 +15,37 @@ test("One euro should be 1.07 dollars", function() {
     const { fromEuroToDollar } = require('./app.js');
 
     // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);
+    const dollars = parseFloat(fromEuroToDollar(3.5));
 
     // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
+    const expected = parseFloat((3.5 * 1.07).toFixed(2));
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe("3.75"); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 test("One euro should be 156.5 yenes", function() {
     // Importo la funcion desde app.js
     const { fromDollarToYen } = require('./app.js');
 
     // Uso la función como debe ser usada
-    const Yen = fromDollarToYen(3.5);
+    const yen = parseFloat(fromDollarToYen(3.5));
 
     // Si 1 euro son 156.5 dólares, entonces 3.5 euros debe ser (3.5 * 156.5) y dividirlo entre el valor de euros en dollar 1.07
-    const expected = 3.5 / 1.07 * 156.5;
+    const expected = parseFloat((3.5 / 1.07 * 156.5).toFixed(2));
 
     // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(3.5)).toBe("511.92"); // 1 euro son 156.5 yenes, entonces 3.5 euros deberían ser = (3.5 * 156.5) dividido entre el valor del dollar en euro 1.07
+    expect(yen).toBe(expected); // 1 euro son 156.5 yenes, entonces 3.5 euros deberían ser = (3.5 * 156.5) dividido entre el valor del dollar en euro 1.07
 })
 test("One euro should be 0.87 pound", function() {
     // Importo la funcion desde app.js
     const { fromYenToPound } = require('./app.js');
 
     // Uso la función como debe ser usada
-    const Yen = fromYenToPound(3.5);
+    const pound = parseFloat(fromYenToPound(3.5));
 
     // Si 1 euro son 156.5 dólares, entonces 3.5 euros debe ser (3.5 * 156.5) y dividirlo entre el valor de euros en dollar 1.07
-    const expected = 3.5 / 156.5 * 0.87;
+    const expected = parseFloat((3.5 / 156.5 * 0.87).toFixed(2));
 
     // Hago mi comparación (la prueba)
-    expect(fromYenToPound(3.5)).toBe("0.02"); // 1 euro son 156.5 yenes, entonces 3.5 euros deberían ser = (3.5 * 156.5) dividido entre el valor del dollar en euro 1.07
+    expect(pound).toBe(expected); // 1 euro son 156.5 yenes, entonces 3.5 euros deberían ser = (3.5 * 156.5) dividido entre el valor del dollar en euro 1.07
 })
